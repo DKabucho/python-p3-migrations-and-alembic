08b927ad64c7263c352db13554f2955e754b4fc2
DKabucho <danielkabucho2004@gmail.com>
2023-09-02 15:42:57 +0300

    complete assignment

---
+++ lib/migrations/versions/e30cd56fce9c_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: e30cd56fce9c
+Revises: 
+Create Date: 2023-09-01 12:47:57.955513
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e30cd56fce9c'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
